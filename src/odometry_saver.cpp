4c7b39164876d3ef495cc582354bc8ed83bd4d7d
Dongmyeong Lee <domlee@utexas.edu>
2024-04-26 02:27:45 -0500

    fix odometry transformation

--- src/odometry_saver.cpp
+++ src/odometry_saver.cpp
@@ -2,13 +2,14 @@
 #include <Eigen/Geometry>
 #include <atomic>
 #include <condition_variable>
+#include <filesystem>
 #include <fstream>
 #include <iostream>
 #include <mutex>
+#include <optional>
 #include <thread>
 
-#include <filesystem>
-
+#include <pcl/common/transforms.h>
 #include <pcl_ros/point_cloud.h>
 #include <ros/ros.h>
 #include <tf/transform_listener.h>
@@ -20,9 +21,23 @@
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 
-template <typename T> void save_data(const std::string &dst_directory, const T &data);
+// Save transformed point cloud
+void save_data(const std::string &dst_directory,
+               const sensor_msgs::PointCloud2ConstPtr &data,
+               const Eigen::Matrix4d &transform) {
+  std::stringstream dst_filename;
+  dst_filename << dst_directory << "/" << data->header.stamp.sec << "_" << std::setw(9)
+               << std::setfill('0') << data->header.stamp.nsec << ".pcd";
 
-template <>
+  pcl::PointCloud<pcl::PointXYZI>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZI>());
+  pcl::fromROSMsg(*data, *cloud);
+
+  pcl::PointCloud<pcl::PointXYZI>::Ptr tf_cloud(new pcl::PointCloud<pcl::PointXYZI>());
+  pcl::transformPointCloud(*cloud, *tf_cloud, transform);
+
+  pcl::io::savePCDFileBinary(dst_filename.str(), *tf_cloud);
+}
+
 void save_data(const std::string &dst_directory,
                const sensor_msgs::PointCloud2ConstPtr &data) {
   std::stringstream dst_filename;
@@ -31,11 +46,10 @@ void save_data(const std::string &dst_directory,
 
   pcl::PointCloud<pcl::PointXYZI>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZI>());
   pcl::fromROSMsg(*data, *cloud);
-
   pcl::io::savePCDFileBinary(dst_filename.str(), *cloud);
 }
 
-template <>
+// Save transformed odometry
 void save_data(const std::string &dst_directory,
                const nav_msgs::OdometryConstPtr &data) {
   std::stringstream dst_filename;
@@ -53,11 +67,12 @@ void save_data(const std::string &dst_directory,
                       .toRotationMatrix();
 
   std::ofstream ofs(dst_filename.str());
+  ofs << std::fixed << std::setprecision(8);
   ofs << odom.matrix();
   ofs.close();
 }
 
-template <typename T> class SaveQueue {
+template <typename T, typename S = std::optional<Eigen::Matrix4d>> class SaveQueue {
 public:
   SaveQueue(const std::string &dst_directory)
       : kill_switch(false), dst_directory(dst_directory) {
@@ -69,9 +84,9 @@ public:
     return queue.size();
   }
 
-  void push(const T &data) {
+  void push(const T &data, const S &aux = std::nullopt) {
     std::lock_guard<std::mutex> lock(mutex);
-    queue.push_back(data);
+    queue.emplace_back(data, aux);
     data_available.notify_one();
   }
 
@@ -85,11 +100,18 @@ private:
         break;
       }
 
-      T data = queue.front();
+      auto [data, aux] = queue.front();
       queue.pop_front();
-
       lock.unlock();
-      save_data(dst_directory, data);
+      if constexpr (std::is_same_v<T, sensor_msgs::PointCloud2ConstPtr>) {
+        if (aux) {
+          save_data(dst_directory, data, *aux); // Use transformation
+        } else {
+          save_data(dst_directory, data); // Handle point clouds without transformation
+        }
+      } else if constexpr (std::is_same_v<T, nav_msgs::OdometryConstPtr>) {
+        save_data(dst_directory, data);
+      }
       lock.lock();
     }
   }
@@ -99,7 +121,7 @@ private:
 
   std::mutex mutex;
   std::condition_variable data_available;
-  std::deque<T> queue;
+  std::deque<std::pair<T, S>> queue;
 
   std::thread thread;
 
@@ -112,7 +134,9 @@ public:
       : nh("~"), endpoint_frame(nh.param<std::string>("endpoint_frame", "base_link")),
         origin_frame(nh.param<std::string>("origin_frame", "map")),
         dst_directory(nh.param<std::string>("dst_directory", "/tmp/odometry")),
-        saved_points(0), saved_odometry(0), points_save_queue(dst_directory),
+        pose_file(nh.param<std::string>("pose_file", "/tmp/odometry/pose.txt")),
+        save_pose_only(nh.param("save_pose_only", false)), saved_points(0),
+        saved_odometry(0), saved_pose(0), points_save_queue(dst_directory),
         odometry_save_queue(dst_directory),
         points_sub(nh.subscribe<sensor_msgs::PointCloud2>(
             "/points", 128, &OdometrySaverNode::points_callback, this)),
@@ -120,24 +144,27 @@ public:
             "/odom", 128, &OdometrySaverNode::odometry_callback, this)),
         tf_listener(ros::DURATION_MAX) {
     std::filesystem::create_directories(dst_directory);
+    std::cout << "Save pose only: " << save_pose_only << std::endl;
 
     timer = nh.createWallTimer(ros::WallDuration(1.0),
                                &OdometrySaverNode::timer_callback, this);
 
-    std::string file_path = dst_directory + "/pose.txt";
-    std::cout << "pose file path: " << file_path << std::endl;
-    if (std::filesystem::exists(file_path)) {
-      std::filesystem::remove(file_path);
+    std::cout << "pose file path: " << pose_file << std::endl;
+    if (std::filesystem::exists(pose_file)) {
+      std::filesystem::remove(pose_file);
+    } else {
+      std::filesystem::create_directories(
+          std::filesystem::path(pose_file).parent_path());
     }
-    pose_file_.open(file_path, std::ios::out | std::ios::app);
-    if (!pose_file_.is_open()) {
-      ROS_ERROR_STREAM("failed to open file: " << file_path);
+    pose_ofs_.open(pose_file, std::ios::out | std::ios::app);
+    if (!pose_ofs_.is_open()) {
+      ROS_ERROR_STREAM("failed to open file: " << pose_file);
     }
   }
 
   ~OdometrySaverNode() {
-    if (pose_file_.is_open()) {
-      pose_file_.close();
+    if (pose_ofs_.is_open()) {
+      pose_ofs_.close();
     }
   }
 
@@ -147,55 +174,67 @@ private:
 
     ROS_INFO_STREAM("queue points:" << points_save_queue.size()
                                     << "  odometry:" << odometry_save_queue.size());
-    ROS_INFO_STREAM("saved points:" << saved_points << "  odometry:" << saved_odometry);
+    ROS_INFO_STREAM("saved points:" << saved_points << "  odometry:" << saved_odometry
+                                    << "  pose:" << saved_pose);
   }
 
   void points_callback(const sensor_msgs::PointCloud2ConstPtr &points_msg) {
+    if (save_pose_only) {
+      return;
+    }
+
+    Eigen::Matrix4d transform =
+        lookup_eigen(endpoint_frame, points_msg->header.frame_id);
+
     saved_points++;
-    points_save_queue.push(points_msg);
+    points_save_queue.push(points_msg, transform);
   }
 
   void odometry_callback(const nav_msgs::OdometryConstPtr &odometry_msg) {
-    saved_odometry++;
-    Eigen::Matrix4d origin2odom =
-        lookup_eigen(odometry_msg->header.frame_id, origin_frame);
-    Eigen::Matrix4d odom2base =
-        lookup_eigen(endpoint_frame, odometry_msg->child_frame_id);
+    Eigen::Matrix4d frame2origin =
+        lookup_eigen(origin_frame, odometry_msg->header.frame_id);
+    Eigen::Matrix4d end2child =
+        lookup_eigen(odometry_msg->child_frame_id, endpoint_frame);
 
     const auto &pose = odometry_msg->pose.pose;
-    Eigen::Matrix4d odombase2odom = Eigen::Matrix4d::Identity();
-    odombase2odom.block<3, 1>(0, 3) =
+    Eigen::Matrix4d child2frame = Eigen::Matrix4d::Identity();
+    child2frame.block<3, 1>(0, 3) =
         Eigen::Vector3d(pose.position.x, pose.position.y, pose.position.z);
-    odombase2odom.block<3, 3>(0, 0) =
+    child2frame.block<3, 3>(0, 0) =
         Eigen::Quaterniond(pose.orientation.w, pose.orientation.x, pose.orientation.y,
                            pose.orientation.z)
             .toRotationMatrix();
 
-    Eigen::Matrix4d result = odom2base * odombase2odom * origin2odom;
+    Eigen::Matrix4d result = frame2origin * child2frame * end2child;
     Eigen::Vector3d t(result.block<3, 1>(0, 3));
     Eigen::Quaterniond q(result.block<3, 3>(0, 0));
 
-    // Save transformed odometry
-    nav_msgs::OdometryPtr transformed_odometry_msg(new nav_msgs::Odometry());
-    *transformed_odometry_msg = *odometry_msg;
-    transformed_odometry_msg->header.frame_id = origin_frame;
-    transformed_odometry_msg->child_frame_id = endpoint_frame;
-    transformed_odometry_msg->pose.pose.position.x = t.x();
-    transformed_odometry_msg->pose.pose.position.y = t.y();
-    transformed_odometry_msg->pose.pose.position.z = t.z();
-    transformed_odometry_msg->pose.pose.orientation.w = q.w();
-    transformed_odometry_msg->pose.pose.orientation.x = q.x();
-    transformed_odometry_msg->pose.pose.orientation.y = q.y();
-    transformed_odometry_msg->pose.pose.orientation.z = q.z();
+    // Save odometry to file
+    if (!save_pose_only) {
+      // Save transformed odometry
+      nav_msgs::OdometryPtr transformed_odometry_msg(new nav_msgs::Odometry());
+      *transformed_odometry_msg = *odometry_msg;
+      transformed_odometry_msg->header.frame_id = origin_frame;
+      transformed_odometry_msg->child_frame_id = endpoint_frame;
+      transformed_odometry_msg->pose.pose.position.x = t.x();
+      transformed_odometry_msg->pose.pose.position.y = t.y();
+      transformed_odometry_msg->pose.pose.position.z = t.z();
+      transformed_odometry_msg->pose.pose.orientation.w = q.w();
+      transformed_odometry_msg->pose.pose.orientation.x = q.x();
+      transformed_odometry_msg->pose.pose.orientation.y = q.y();
+      transformed_odometry_msg->pose.pose.orientation.z = q.z();
 
-    odometry_save_queue.push(transformed_odometry_msg);
+      saved_odometry++;
+      odometry_save_queue.push(transformed_odometry_msg);
+    }
 
     // Save pose to file
-    if (pose_file_.is_open()) {
-      pose_file_ << std::fixed << std::setprecision(6)
-                 << odometry_msg->header.stamp.toSec() << " " << std::fixed
-                 << std::setprecision(8) << t.x() << " " << t.y() << " " << t.z() << " "
-                 << q.w() << " " << q.x() << " " << q.y() << " " << q.z() << std::endl;
+    if (pose_ofs_.is_open()) {
+      pose_ofs_ << std::fixed << std::setprecision(6)
+                << odometry_msg->header.stamp.toSec() << " " << std::fixed
+                << std::setprecision(8) << t.x() << " " << t.y() << " " << t.z() << " "
+                << q.w() << " " << q.x() << " " << q.y() << " " << q.z() << std::endl;
+      saved_pose++;
     }
   }
 
@@ -224,11 +263,15 @@ private:
 
   int saved_points;
   int saved_odometry;
+  int saved_pose;
 
   std::string dst_directory;
+  std::string pose_file;
+  std::ofstream pose_ofs_;
+  bool save_pose_only;
+
   SaveQueue<sensor_msgs::PointCloud2ConstPtr> points_save_queue;
   SaveQueue<nav_msgs::OdometryConstPtr> odometry_save_queue;
-  std::ofstream pose_file_;
 
   ros::Subscriber points_sub;
   ros::Subscriber odometry_sub;
